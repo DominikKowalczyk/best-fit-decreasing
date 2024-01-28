a37491bd2e72563761b709bfcdd402a1a6c7d498
DominikKowalczyk <43239883+DominikKowalczyk@users.noreply.github.com>
2024-01-28 18:57:15 +0100

    languege changed to en in UI

--- UI.js
+++ UI.js
@@ -1,6 +1,6 @@
 function onOpen() {
   SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
       .createMenu('Custom Menu')
-      .addItem('Oblicz', 'bestFitDecreasing')
+      .addItem('Solve', 'bestFitDecreasing')
       .addToUi();
 }
