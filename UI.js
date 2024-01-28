0b1bb3b328912f52c0b19506cf87a561e7c73d0a
DominikKowalczyk <43239883+DominikKowalczyk@users.noreply.github.com>
2024-01-28 20:40:23 +0100

    'printOutput' function call from UI

--- UI.js
+++ UI.js
@@ -2,5 +2,6 @@ function onOpen() {
   SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
       .createMenu('Custom Menu')
       .addItem('Solve', 'bestFitDecreasing')
+      .addItem('Print', 'printOutput')
       .addToUi();
 }
